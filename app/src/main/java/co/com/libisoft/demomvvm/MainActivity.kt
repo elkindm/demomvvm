09a3f63322d383c741f6b4745bd0b6c923b939e9
elkindm <elkinduranm@gmail.com>
2021-12-03 09:57:34 -0500

    prueba de commit

--- app/src/main/java/co/com/libisoft/demomvvm/MainActivity.kt
+++ app/src/main/java/co/com/libisoft/demomvvm/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
+    //prueba git
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
